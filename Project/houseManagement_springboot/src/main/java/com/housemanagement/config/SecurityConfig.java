d62f225145c97655bed29085a90ab1627f2d239f
Bidyut <86011436+bidyut0810@users.noreply.github.com>
2022-09-26 19:32:13 +0530

    Update SecurityConfig.java

--- Project/houseManagement_springboot/src/main/java/com/housemanagement/config/SecurityConfig.java
+++ Project/houseManagement_springboot/src/main/java/com/housemanagement/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/authenticate","/addUser")
+        http.csrf().disable().authorizeRequests().antMatchers("/authenticate","/addUser","/Signup/{\\^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$}/{\\d+}")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().disable();
